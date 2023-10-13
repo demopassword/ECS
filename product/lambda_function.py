9a537f902000e64d8aab41942cbbd61528bb07a7
demopassword <145639874+demopassword@users.noreply.github.com>
2023-10-13 10:46:44 +0900

    Create lambda_function.py

---
+++ product/lambda_function.py
@@ -0,0 +1,33 @@
+import json
+import boto3
+import time
+
+cf = boto3.client('cloudfront')
+code_pipeline = boto3.client('codepipeline')
+
+DISTRIBUTION_ID = "EX46VUFBQTI6A"
+
+def create_invalidation():
+    res = cf.create_invalidation(
+        DistributionId=DISTRIBUTION_ID,
+        InvalidationBatch={
+            'Paths':{
+                'Quantity': 1,
+                'Items': [
+                    '/v1/product*',
+                ]
+            },
+            'CallerReference': str(time.time())
+        }
+    )
+    invalidation_id = res['Invalidation']['Id']
+    return invalidation_id
+
+def lambda_handler(event, context):
+    id = create_invalidation()
+    job = event['CodePipeline.job']['id']
+    
+    if id:
+        return code_pipeline.put_job_success_result(jobId=job)
+    else:
+        return code_pipeline.put_job_failure_result(jobId=job, failureDetails={'message': message, 'type': 'JobFailed'})
